b25fe424d781c7589f4556de5b739804c36d833b
yesheytenzin <yesheytenzin09@mail.com>
2026-09-01 23:30:21 +0600

    feat: primeagen 1,3,4 — lua_ls config, mason handlers, stylua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -11,6 +11,7 @@ return {
         sql = { "sql_formatter" },
         c = { "clang_format" },
         cpp = { "clang_format" },
+        lua = { "stylua" },
       },
       formatters = {
         rubocop = { timeout_ms = 5000 },
@@ -37,6 +38,42 @@ return {
   },
   {
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "rubocop", "yamlfmt", "jq", "sql-formatter", "clang-format" } },
+    opts = { ensure_installed = { "rubocop", "yamlfmt", "jq", "sql-formatter", "clang-format", "stylua", "lua-language-server" } },
+  },
+  -- Primeagen 1 + 3 + 4: mason-lspconfig handlers pattern with lua_ls config
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      ensure_installed = { "lua_ls" },
+      handlers = {
+        -- default handler
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end,
+        -- Primeagen lua_ls: LuaJIT, vim globals, workspace library, 2-space format
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup({
+            settings = {
+              Lua = {
+                runtime = { version = "LuaJIT" },
+                diagnostics = { globals = { "vim" } },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
+                },
+                format = {
+                  enable = true,
+                  defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "2",
+                  },
+                },
+              },
+            },
+          })
+        end,
+      },
+    },
   },
 }
