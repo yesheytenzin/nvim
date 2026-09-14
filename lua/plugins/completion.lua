e75cb9a07b0f5132f951623347c64fb539045f93
yesheytenzin <yesheytenzin09@mail.com>
2026-09-14 18:21:36 +0600

    tab conflict fix

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -35,14 +35,14 @@ return {
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.confirm({ select = true })
-            elseif luasnip.expand_or_jumpable() then
+            elseif luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
             else
               fallback()
             end
           end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
+            if luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
